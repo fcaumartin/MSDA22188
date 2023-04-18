516204ea23cc72cbd45f48017f99f049092671bc
frc <frc@bienvu.net>
2023-04-18 16:47:02 +0200

    doctrine

--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -2,33 +2,71 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Categorie;
+use App\Repository\PlatRepository;
+use App\Repository\CategorieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(PlatRepository $repo): Response
     {
+        $liste = $repo->findAll();
+
         return $this->render('home/index.html.twig', [
-            
+            "liste" => $liste
         ]);
     }
 
-    #[Route('/home2', name: 'app_home2')]
-    public function home2(): Response
+    #[Route('/categories', name: 'app_home2')]
+    public function home2(CategorieRepository $repo): Response
     {
+        $liste = $repo->findAll();
+
         return $this->render('home/home2.html.twig', [
-            
+            "liste" => $liste    
         ]);
     }
 
-    #[Route('/home3', name: 'app_home3')]
-    public function home3(): Response
+    #[Route('/details/{id}', name: 'app_home3')]
+    public function home3($id, PlatRepository $repo): Response
     {
+        
+        $liste = $repo->findBy([ "categorie" => $id ]);
+
+        // dd($liste);
+
         return $this->render('home/home3.html.twig', [
-            
+            "liste" => $liste
+        ]);
+    }
+
+    #[Route('/details2/{id}', name: 'app_home4')]
+    public function home4($id, CategorieRepository $repo): Response
+    {
+        
+        $categorie = $repo->find($id);
+        $liste = $categorie->getPlats();
+
+        // dd($liste);
+
+        return $this->render('home/home3.html.twig', [
+            "liste" => $liste
+        ]);
+    }
+
+    #[Route('/details3/{categorie}', name: 'app_home5')]
+    public function home5(Categorie $categorie): Response
+    {
+        
+
+        // dd($liste);
+
+        return $this->render('home/home3.html.twig', [
+            "categorie" => $categorie
         ]);
     }
 }
--- symfony/src/DataFixtures/AppFixtures.php
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -19,16 +19,34 @@ class AppFixtures extends Fixture
         $cat2->setNom("Burger");
         $manager->persist($cat2);
 
+        $cat3 = new Categorie();
+        $cat3->setNom("Pasta");
+        $manager->persist($cat3);
+
 
         $p1 = new Plat();
-        $p1->setNom("Fromagio");
+        $p1->setNom("Fromagio 1");
         $p1->setImage("pizza1.png");
-        $manager->persist($p1);
-        
-
         $p1->setCategorie($cat1);
-        // ou
-        $cat1->addPlat($p1);
+        $manager->persist($p1);
+
+        $p2 = new Plat();
+        $p2->setNom("Fromagio 2");
+        $p2->setImage("pizza1.png");
+        $p2->setCategorie($cat1);
+        $manager->persist($p2);
+
+        $p3 = new Plat();
+        $p3->setNom("Fromagio 3");
+        $p3->setImage("pizza1.png");
+        $p3->setCategorie($cat1);
+        $manager->persist($p3);
+
+        $p4 = new Plat();
+        $p4->setNom("Fromagio 4");
+        $p4->setImage("pizza1.png");
+        $p4->setCategorie($cat1);
+        $manager->persist($p4);
 
         $manager->flush();
     }
